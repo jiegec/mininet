696f51366b930315b14a5113a1afde403c9643d8
jiegec <jiegec@v2.pc.thu-skyworks.org>
2018-07-24 10:35:37 +0800

    Track kernel size in repo

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,3 +3,5 @@ LINUX=~/linux-4.17.6/
 cp $LINUX/.config kernel_config
 make -C $LINUX -j50
 make -C $LINUX modules -j50
+wc -c <$LINUX/arch/x86/boot/bzImage > kernel_size
+source ./build_initramfs.sh
